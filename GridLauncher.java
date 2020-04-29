125175c3d112aab222dfccaa285657aa4ed6b1f6
M-McNERER <39343394+M-McNERER@users.noreply.github.com>
2020-04-28 20:49:46 -0500

    grid launcher implements abstract IconModel class

---
+++ GridLauncher.java
@@ -0,0 +1,78 @@
+package com.innovateatc.autoscreen.fragments;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.LayoutInflater;
+
+import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.GridLayout;
+
+import com.innovateatc.autoscreen.R;
+import com.innovateatc.autoscreen.interfaces.Launcher;
+import com.innovateatc.autoscreen.others.Event;
+import com.innovateatc.autoscreen.models.IconModel;
+
+import java.util.ArrayList;
+
+public class GridLauncher extends ScreeningFragment implements Launcher {
+
+    private static final String TAG = "GridLauncher";
+    private ArrayList<IconModel> models = new ArrayList<>();
+    private GridLayout gridLayout;
+
+
+    public static GridLauncher newInstance(IconModel... params) {
+        GridLauncher gridLauncher = new GridLauncher();
+
+        for (IconModel icon : params) {
+            gridLauncher.models.add(icon);
+        }
+        return gridLauncher;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.frame_launcher, container, false);
+
+        /*Inflate grid layout and add it to GridLauncher */
+        gridLayout = view.findViewById(R.id.grid_launcher);
+
+        for (IconModel icon : models) {
+            this.addChild(icon);
+        }
+        return view;
+    }
+
+    public void addChild(IconModel iconModel) {
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.layout_tile, gridLayout, false);
+
+        /*format Icons in IconModel */
+        ImageView iconView = view.findViewById(R.id.image);
+        TextView captionView = view.findViewById(R.id.caption);
+        captionView.setText(iconModel.getCaption());
+        iconView.setImageResource(iconModel.getDrawableID());
+
+        /*Preform an action when clicked */
+        view.setOnClickListener(v -> iconModel.performAction());
+        gridLayout.addView(view);
+    }
+    public void removeChild(int index) {
+        gridLayout.removeViewAt(index);
+    }
+
+    public void onNextListener(Event event) {
+    }
+
+    public void onPrevListener(Event event) {
+    }
+}
+
+
